b9b2665b6e22fd070a54ed7af41a3855fe2cb7e2
Goldenlion5648 <coboudinot@gmail.com>
2023-10-20 19:51:34 -0400

    add hexagon hacker

---
+++ hexagonHacker/mkin.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+import random
+from itertools import permutations
+from string import ascii_lowercase
+case_num = int(input())
+# 0 and 1 are the sample cases
+if case_num == 0:
+    print(4)
+    print(2, 'NSNENSNSNW')
+    print(2, 'NSNENSNS')
+    print(2, 'NNNN')
+    print(3, 'NSNSNS')
+elif case_num == 1:
+    print(2)
+    print(1, 'NS')
+    print(4, 'NESESSWNWNSNSNSNSNSNS')
+else:
+    # output what should be read in as input by
+    # contestant code
+    test_case_count = random.randint(10, 20)
+    if case_num > 25:
+        test_case_count = random.randint(5, 10)
+    print(test_case_count)
+    for t in range(test_case_count):
+        n = random.randint(2,5)
+        path_len = random.randint(1, 20) * 10
+        if case_num > 25 and t == 0:
+            path_len = 100_000
+        path_taken = random.choices(["NW","N","NE","SE","S","SW"], k=path_len)
+        print(n, ''.join(path_taken))
+
+        
+        
+        
+    
---
+++ hexagonHacker/solutions/sol.py
@@ -0,0 +1,38 @@
+import re
+from collections import defaultdict
+# y change, followed by x change, 
+# up being negative y, left is negative x
+move_to_offset = {
+    "NW": (-.5, -1),
+    "N": (-1, 0),
+    "NE": (-.5, 1),
+    "SE": (.5, 1),
+    "S": (1, 0),
+    "SW": (.5, -1),
+}
+
+def run_test_case():
+    n, a = input().split() 
+    n = int(n)
+    matches = re.findall(r'NW|NE|SE|SW|N|S', a)
+    assert ''.join(matches) == a
+    seen = defaultdict(int)
+    pos_y = 0
+    pos_x = 0
+    seen[pos_y, pos_x] += 1
+    for match in matches:
+        dy, dx = move_to_offset[match]
+        pos_y += dy
+        pos_x += dx
+        seen[pos_y, pos_x] += 1
+    num_been_to_more_than_once = sum(1 for x in seen if seen[x] > 1) 
+    if num_been_to_more_than_once > 0 and num_been_to_more_than_once % n == 0:
+        print("yes", num_been_to_more_than_once)
+    else:
+        print("no")
+    
+
+cases = int(input())
+for t in range(cases):
+    run_test_case()
+        
